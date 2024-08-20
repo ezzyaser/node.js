a6975e9f507d33d27f798ad33da5f5e97fc5d732
ezzyaser <165480784+ezzyaser@users.noreply.github.com>
2024-08-20 14:43:07 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+// const http = require('http');
+
+// const server = http.createServer( (req, res) => {
+
+//     res.statusCode = 200 ;
+//     res.setHeader ('content-type' , 'text/html');
+//     res.end ('<h1>Hello every  body </h1>', 'utf-8');
+// });
+
+// server.listen (3000, () => {
+
+//     console.log ('Server running.....');
+
+// })
+
+console.log('hello world')
